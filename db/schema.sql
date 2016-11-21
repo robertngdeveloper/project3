3785a06a8f087f1669da70cfff170fb03d816fdd
Nicky Kwan <nickykwn@gmail.com>
2016-11-21 16:56:14 -0500

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,24 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS images;
+-- DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS location;
+-- DROP TABLE IF EXISTS meteors;
 
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  location INT NOT NULL DEFAULT 0
+  zipcode INT NOT NULL DEFAULT 10010
 );
 
-CREATE TABLE images (
-  images_id SERIAL PRIMARY KEY,
-  weight INT NOT NULL DEFAULT 0,
-  imageurl VARCHAR(255)
+CREATE TABLE meteors (
+  meteor_id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE comments (
+  comment_id SERIAL PRIMARY KEY,
+  meteor_id INTEGER REFERENCES meteors(meteor_id),
+  user_id INTEGER REFERENCES users(user_id),
+  comment TEXT
 );
 
 COMMIT;
