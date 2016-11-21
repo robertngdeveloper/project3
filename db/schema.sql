9d0eb7df0fd0da80c28bcdec2c7667915917d95d
Nicky Kwan <nickykwn@gmail.com>
2016-11-21 17:02:13 -0500

    schema changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,13 @@
 BEGIN;
 
 -- DROP TABLE IF EXISTS users;
--- DROP TABLE IF EXISTS location;
--- DROP TABLE IF EXISTS meteors;
 
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  zipcode INT NOT NULL DEFAULT 10010
+  search TEXT,
+  images TEXT
 );
 
-CREATE TABLE meteors (
-  meteor_id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE comments (
-  comment_id SERIAL PRIMARY KEY,
-  meteor_id INTEGER REFERENCES meteors(meteor_id),
-  user_id INTEGER REFERENCES users(user_id),
-  comment TEXT
-);
 
 COMMIT;
